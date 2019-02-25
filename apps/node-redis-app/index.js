fc6ca5e8478ef8b19f08a21064771dc9c2b70ae5
naor2205@gmail.com <naor2205@gmail.com>
2019-02-25 17:07:00 +0100

    adding part 5-6 notes

--- apps/node-redis-app/index.js
+++ apps/node-redis-app/index.js
@@ -1,17 +1,33 @@
-const express = require('express');
-const redis = require('redis');
+const express = require('express')
+const redis = require('redis')
 
-const app = express();
-const client = redis.createClient();
-client.set('visits', 0);
+const app = express()
+
+const VISITS_REDIS_KEY = 'visits'
+const client = redis.createClient({
+  host: 'redis-server',
+  port: 6379
+})
+
+const initIfNull = () => {
+  client.get(VISITS_REDIS_KEY, (err, visits) => {
+    if (visits === null) client.set(VISITS_REDIS_KEY, -999)
+    console.log(err)
+    console.log(err)
+    console.log(err)
+    console.log(err)
+  })
+}
+initIfNull()
 
 app.get('/', (req, res) => {
-  client.get('visits', (err, visits) => {
-    res.send('Number of visits is ' + visits);
-    client.set('visits', parseInt(visits) + 1);
-  });
-});
+  client.get(VISITS_REDIS_KEY, (err, visits) => {
+    process.exit(0)
+    res.send('Number of visits is ' + visits)
+    client.set(VISITS_REDIS_KEY, parseInt(visits) + 1)
+  })
+})
 
 app.listen(8081, () => {
-  console.log('Listening on port 8081');
-});
+  console.log('Listening on port 8081')
+})
---
+++ docker-compose.md
@@ -0,0 +1,6 @@
+restart policiyes:
+no - if an container crush dont start
+always - any error code 
+on faliure  - only restart on err
+
+
