85d8423b88dff89f22208e084c463daaac4dc925
naor2205@gmail.com <naor2205@gmail.com>
2019-03-03 22:09:10 +0200

    travis infra

--- travis-ci-with-docker/index.js
+++ travis-ci-with-docker/index.js
@@ -1,1 +1,3 @@
-console.log("app")
+let variableName ="small medium large" 
+let x = variableName.replace(/\small/g , 'word').replace(/\medium/g , 'word').replace(/\large/g , 'word').replace(/\x_large/g , 'word');
+console.log(x)
