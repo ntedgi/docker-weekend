9923075c8ebe5eb82f614c261e1b77344a72049d
naor2205@gmail.com <naor2205@gmail.com>
2019-03-30 13:16:52 +0300

    remove index

--- travis-ci-with-docker/index.js
+++
@@ -1,3 +0,0 @@
-let variableName ="small medium large" 
-let x = variableName.replace(/\small/g , 'word').replace(/\medium/g , 'word').replace(/\large/g , 'word').replace(/\x_large/g , 'word');
-console.log(x)
